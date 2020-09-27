86bc9d5a96409761457fed906e2d3f7a0ae5b845
Gavin Loynes <70937616+Satgav@users.noreply.github.com>
2020-09-27 16:22:19 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,110 @@
+//var myImage = document.getElementById("img1");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 500);
+//
+//var myImage2 = document.getElementById("img2");
+//var imageArray2 = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex2 = 0;
+//function changeImage2() {
+//    myImage2.setAttribute("src", imageArray2[imageIndex2]);
+//    imageIndex++;
+//    if (imageIndex2 >= imageArray2.length) {
+//        imageIndex2 = 0;
+//    }
+//}
+//
+//setInterval(changeImage2, 500);
+//var myImage = document.getElementById("img3");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//var myImage = document.getElementById("img4");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//var myImage = document.getElementById("img5");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//var myImage = document.getElementById("img6");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//var myImage = document.getElementById("img7");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//var myImage = document.getElementById("img8");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
+//
+//var myImage = document.getElementById("img9");
+//var imageArray = ["http://i.imgur.com/qK42fUu.jpg" , "sunset.jpg"];
+//var imageIndex = 0;
+//function changeImage() {
+//    myImage.setAttribute("src", imageArray[imageIndex]);
+//    imageIndex++;
+//    if (imageIndex >= imageArray.length) {
+//        imageIndex = 0;
+//    }
+//}
+//
+//setInterval(changeImage, 5000);
